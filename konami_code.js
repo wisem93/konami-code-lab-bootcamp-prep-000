fc88531d22afa47dec4d24f80f478ac1d5248465
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-05-08 03:21:02 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,19 +17,14 @@ let index = 0;
 function init() {
   // your code here
   document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    console.log(key);
     
-  });
-  const key = e.key;
-  
-  if (key === codes[index]) {
-    index++;
-    
-    if (index === codes.length) {
-      alert("You must know the secret code!");
-      
+    if (code[index] === key) {
+      ++index;
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
-  }
+    
+  });
 }
